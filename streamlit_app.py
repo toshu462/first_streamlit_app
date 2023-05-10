ab7243bc2ca550610567a17e6b6f35f6c4619618
toshu462 <99402375+toshu462@users.noreply.github.com>
2023-05-10 14:03:22 +0530

    Update streamlit_app.py
    add header of menu and dishes in terms of text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinnr')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 Blueberry Oatmeal')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
+streamlit.text('Hard Boiled Free Range Egg')
